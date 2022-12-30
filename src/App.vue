b8ff1c612ef978d98108f1cc9e84778ce6f51426
elkCODE <121523529+elkCODE@users.noreply.github.com>
2022-12-30 02:25:52 +0100

    Init
    Repository init files and first adjusment. No children harmed.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import front from "./components/front.vue";
+import footer from "./components/footer.vue";
+</script>
+
+<template>
+  <front />
+  <footer />
+</template>
---
+++ src/components/front.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="front-wrapper">
+    <div class="front-container">
+      <div class="front-navbar">
+        <span></span>
+        <span></span>
+        <div class="dots">
+          <i class="fa-solid fa-ellipsis-vertical"></i
+          ><i class="fa-solid fa-ellipsis-vertical"></i
+          ><i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+      </div>
+      <div class="front-search">
+        <img src="" alt="" />
+        <input type="text" />
+        <button>Sök på google</button>
+        <button>Jag har tur</button>
+      </div>
+    </div>
+  </div>
+</template>
